9a526e359e7cb0032764eb856453d156f67bbdc4
Noam Teyssier <22600644+noamteyssier@users.noreply.github.com>
2025-02-23 18:55:14 -0800

    chore: update examples

--- examples/io.rs
+++ examples/io.rs
@@ -5,15 +5,22 @@ use std::{
 
 use anyhow::Result;
 use paraseq::fastq;
-use vbinseq::{reader::RecordBlock, MmapReader, VBinseqHeader, VBinseqWriter};
+use vbinseq::{MmapReader, VBinseqHeader, VBinseqWriter};
 
 fn write_set(input_filepath: &str, output_filepath: &str) -> Result<()> {
+    let write_quality = true;
+    let compress = true;
+
     let in_handle = match_input(input_filepath)?;
     let mut reader = fastq::Reader::new(in_handle);
     let mut rset = fastq::RecordSet::default();
 
+    eprintln!(
+        "Writing sequences to {} (compress: {}, with_quality: {})",
+        output_filepath, compress, write_quality
+    );
     let handle = File::create(output_filepath).map(BufWriter::new)?;
-    let header = VBinseqHeader::new(true, true);
+    let header = VBinseqHeader::new(write_quality, compress);
     let mut writer = VBinseqWriter::new(handle, header)?;
 
     let mut rnum = 0;
@@ -22,7 +29,11 @@ fn write_set(input_filepath: &str, output_filepath: &str) -> Result<()> {
             let record = record?;
             let seq = record.seq();
             let qual = record.qual();
-            writer.write_nucleotides_quality(rnum, seq, qual)?;
+            if write_quality {
+                writer.write_nucleotides_quality(rnum, seq, qual)?;
+            } else {
+                writer.write_nucleotides(rnum, seq)?;
+            }
             rnum += 1;
         }
     }
@@ -35,15 +46,22 @@ fn read_set(filepath: &str) -> Result<()> {
     eprintln!("Reading sequences from {}", filepath);
 
     let mut reader = MmapReader::new(filepath)?;
-    let mut block = RecordBlock::new();
+    let mut block = reader.new_block();
 
     let mut n_records = 0;
+    let mut n_blocks = 0;
+    let mut dbuf = Vec::new();
     while reader.read_block_into(&mut block)? {
         n_records += block.n_records();
+        for record in block.iter() {
+            record.decode_into(&mut dbuf)?;
+            dbuf.clear();
+        }
+        n_blocks += 1;
+        eprintln!("Read {} records from block {}", block.n_records(), n_blocks);
     }
 
-    eprintln!("Read {} records", n_records);
-
+    eprintln!("Read {} records from {} blocks", n_records, n_blocks);
     Ok(())
 }
 
@@ -53,11 +71,15 @@ fn match_input(filepath: &str) -> Result<Box<dyn Read + Send>> {
 }
 
 pub fn main() -> Result<()> {
-    let in_filepath = "./data/out.fq.zst";
-    let out_filepath = "./data/out.vbq";
+    let in_filepath = std::env::args()
+        .nth(1)
+        .unwrap_or("./data/out.fq.zst".to_string());
+    let out_filepath = std::env::args()
+        .nth(2)
+        .unwrap_or("./data/out.vbq".to_string());
 
-    write_set(in_filepath, out_filepath)?;
-    read_set(out_filepath)?;
+    write_set(&in_filepath, &out_filepath)?;
+    read_set(&out_filepath)?;
 
     Ok(())
 }
